42f983af4a78edc50e0eb2ea728bbcaf898cce59
Federico <fredolds180@gmail.com>
2025-08-26 09:19:00 -0500

    feat: crear proyecto base en ASP.NET Core Web API

---
+++ Practica-1-Integracion-Continua/Practica-1-Integracion-Continua/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace Practica_1_Integracion_Continua
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
