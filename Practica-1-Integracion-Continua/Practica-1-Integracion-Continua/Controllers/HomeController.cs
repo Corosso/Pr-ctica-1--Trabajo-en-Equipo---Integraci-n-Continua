9b748ab68dbd1bd41da46d154529854ba26c2a51
AlejaAlvarez <83793246+AlejaAlvarez@users.noreply.github.com>
2025-08-27 21:48:47 -0500

    simulando nuevo error

--- Practica-1-Integracion-Continua/Practica-1-Integracion-Continua/Controllers/HomeController.cs
+++ Practica-1-Integracion-Continua/Practica-1-Integracion-Continua/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace Practica_1_Integracion_Continua
     {
         public string Index()
         {
-            return "Deshacer y revertir Cambios git bash corregido";
+            return "Nuevo mensaje de simulación de error";
         }
     }
 }
